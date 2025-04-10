410ccbdb57f73b72513d57740cc8b844b877faee
rabibehera14 <44116411+rabibehera14@users.noreply.github.com>
2025-04-10 11:07:55 +0530

    Add files via upload

---
+++ Assignment2forloop.py
@@ -0,0 +1,10 @@
+num = range(1,51)
+sum1 = 0
+for i in num:
+    # sum = 0
+    sum1 = sum1+i
+if(i==50):
+    print("sum of integers from 1-50 is",sum1)
+
+
+
---
+++ Assignment2ifelse.py
@@ -0,0 +1,13 @@
+a=int(input("Enter a number"))
+
+b=a%2
+
+if b==0:
+    print("a is even number")
+else:
+    print("a is odd number")
+
+
+
+
+
